cba8d173b068eacc4838cf21f7bae2448177d13a
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2026-02-01 06:07:52 +0530

    Update login method and restructure social media display
    Refactor login button to use Google authentication and reorganize social media images into a container.

--- Home.py
+++ Home.py
@@ -67,7 +67,7 @@ def login_screen():
         st.write(" ")
     with v2:
         if st.button("Log in with Google"):
-            st.login()
+            st.login("google")
     with v3:
         st.write(" ")
 
@@ -161,20 +161,21 @@ if st.user.is_logged_in:
 st.write("-------------------------------------------------------------------")
 
 
-
-column1, column2, column3, column4, column5 = st.columns([1, 1, 1, 2, 1])
-with column1:
-    st.image(ytube, '[Ledgr\'s YouTube Channel](%s)' % url_ytube, width=60)
-with column2:
-    st.image(fbook, '[Ledgr\'s FaceBook Page ](%s)' % url_fbook, width=60)
-with column3:
-    st.image(linkedin,  '[Our LinkedIn Page ](%s)' % url_linkedin, width=60)
-with column4:
-    st.write(" ")
-    st.image(ledgrblog,  '[Ledgr\'s Blog ](%s)' % url_blog)
-    st.write(" ")
-with column5:
-    st.image(insta,  '[Ledgr\'s @ Instagram ](%s)' % url_insta, width=60)
+with st.container():
+    
+    column1, column2, column3, column4, column5 = st.columns([1, 1, 1, 2, 1])
+    with column1:
+        st.image(ytube, '[Ledgr\'s YouTube Channel](%s)' % url_ytube, width=60)
+    with column2:
+        st.image(fbook, '[Ledgr\'s FaceBook Page ](%s)' % url_fbook, width=60)
+    with column3:
+        st.image(linkedin,  '[Our LinkedIn Page ](%s)' % url_linkedin, width=60)
+    with column4:
+        st.write(" ")
+        st.image(ledgrblog,  '[Ledgr\'s Blog ](%s)' % url_blog)
+        st.write(" ")
+    with column5:
+        st.image(insta,  '[Ledgr\'s @ Instagram ](%s)' % url_insta, width=60)
 # # ###################################################################
 with st.container():
     f9, f10, f11 = st.columns([1, 5, 1])
